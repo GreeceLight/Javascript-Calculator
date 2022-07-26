1b2078adb439520c229f6e3c23ac33fb50a2fabc
Weirdddude <weirdoproductions13@gmail.com>
2022-07-26 14:55:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function add(adNum1, adNum2){return adNum1 + adNum2;}
+function subtract(subNum1, subNum2){return subNum1 - subNum2;}
+function multiply(mulNum1, mulNum2){return mulNum1 * mulNum2;}
+function power(powNum1, powNum2){return powNum1 * powNum2;}
+function divide(divNum1, divNum2){return divNum1 / divNum2;}
