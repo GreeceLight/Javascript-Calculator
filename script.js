ac07ff4e1996d70d1faf73bb12ac083e0b17b961
Weirdddude <weirdoproductions13@gmail.com>
2022-07-27 11:29:51 +0200

    added Screen char limit, clear function,reset func

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+//Grabbing all the buttons to be used during the calculations
 const screenDisplay = document.querySelector("#screentext");
+const buttons = document.querySelectorAll("#number");
+const clearButton = document.querySelector("#clear");
+//Object that will be used to calculate the expressions
+expression = {
+    num1 : 0,
+    num2 : 0
+}
+//All Event listeners
+//Clears all variables from calculator and clears the screen
+clearButton.addEventListener('click', function(){
+    reset();
+    screenDisplay.textContent = ''
+})
 
-const buttons = document.querySelectorAll(".button");
-
+//Makes pressing all number related buttons to show on screen
 buttons.forEach(button => button.addEventListener('click', function(){
-    screenDisplay.textContent = screenDisplay.textContent.concat(button.textContent);
+    if(screenDisplay.textContent.length != 11){
+        screenDisplay.textContent= screenDisplay.textContent.concat(button.textContent);
+    }
+    else return
 }))
 
 
@@ -16,3 +32,4 @@ function subtract(subNum1, subNum2){return subNum1 - subNum2;}
 function multiply(mulNum1, mulNum2){return mulNum1 * mulNum2;}
 function power(powNum1, powNum2){return powNum1 * powNum2;}
 function divide(divNum1, divNum2){return divNum1 / divNum2;}
+function reset(){expression = {num1 : 0,num2 : 0}}
