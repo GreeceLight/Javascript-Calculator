14fe38b801a089cb89287edff0de503a335383b5
Weirdddude <weirdoproductions13@gmail.com>
2022-07-28 12:24:38 +0200

    Fully Implemented add and subtract to console log

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 const screenDisplay = document.querySelector("#screentext");
 const buttons = document.querySelectorAll("#number");
 const clearButton = document.querySelector("#clear");
+const addButton = document.querySelector("#add");
+const subtractButton = document.querySelector("#subtract");
+const multiplyButton = document.querySelector("#multiply");
+const powerButton = document.querySelector("#power");
+const divideButton = document.querySelector("#divide");
+const equalButton = document.querySelector("#equals");
 //Object that will be used to calculate the expressions
 expression = {
     num1 : 0,
-    num2 : 0
+    num2 : 0,
+    sign : "add"
 }
 //All Event listeners
 //Clears all variables from calculator and clears the screen
@@ -22,14 +29,89 @@ buttons.forEach(button => button.addEventListener('click', function(){
     else return
 }))
 
+addButton.addEventListener('click', function(){
+    add(expression.num1, expression.num2);
+})
+
+subtractButton.addEventListener('click', function(){
+    subtract(expression.num1, expression.num2);
+})
+
+multiplyButton.addEventListener('click', function(){
+    multiply(expression.num1, expression.num2);
+})
+
+powerButton.addEventListener('click', function(){
+    power(expression.num1, expression.num2);
+})
+
+divideButton.addEventListener('click', function(){
+    divide(expression.num1, expression.num2);
+})
+
+equalButton.addEventListener('click', function(){
+    screenDisplay.textContent = equal(expression.sign);
+})
 
 
-
-
-
-function add(adNum1, adNum2){return adNum1 + adNum2;}
-function subtract(subNum1, subNum2){return subNum1 - subNum2;}
-function multiply(mulNum1, mulNum2){return mulNum1 * mulNum2;}
-function power(powNum1, powNum2){return powNum1 * powNum2;}
-function divide(divNum1, divNum2){return divNum1 / divNum2;}
+function add(adnum1, adnum2){
+    adnum1 = parseInt(screenDisplay.textContent) + adnum2;
+    adnum2 = adnum1;
+    expression.num1 = adnum1;
+    expression.num2 = adnum2;
+    expression.sign = "add";
+    screenDisplay.textContent = '';
+}
+function subtract(subnum1, subnum2){
+    subnum1 = parseInt(screenDisplay.textContent) - subnum2;
+    subnum2 = subnum1;
+    expression.num1 = -subnum1;
+    expression.num2 = subnum2;
+    expression.sign = "subtract"
+    screenDisplay.textContent = '';
+}
+function multiply(mulNum1, mulNum2){
+    if(mulNum1 == 0) mulNum2 = 1;
+    mulNum1 = parseInt(screenDisplay.textContent) * mulNum2;
+    mulNum2 = mulNum1;
+    expression.num1 = mulNum1;
+    expression.num2 = mulNum2;
+    expression.sign = "multiply"
+    screenDisplay.textContent = '';
+}
+function power(powNum1, powNum2){
+    powNum1 = parseInt(screenDisplay.textContent) ** powNum2;
+    powNum2 = powNum1;
+    expression.num1 = powNum1;
+    expression.num2 = powNum2;
+    expression.sign = "power"
+    screenDisplay.textContent = '';
+}
+function divide(divNum1, divNum2){
+    divNum1 = parseInt(screenDisplay.textContent) / divNum2;
+    divNum2 = divNum1;
+    expression.num1 = divNum1;
+    expression.num2 = divNum2;
+    expression.sign = "divide"
+    screenDisplay.textContent = '';
+}
+function equal(){
+    switch(expression.sign){
+        case "add":
+            add(expression.num1, expression.num2);
+            console.log(expression.num1);
+        case "subtract":
+            subtract(expression.num1, expression.num2);
+            console.log(expression.num1);
+        case "multiply":
+            multiply(expression.num1, expression.num2);
+            console.log(expression.num1);
+        case "divide":
+            divide(expression.num1, expression.num2);
+            console.log(expression.num1);
+        case "power":
+            power(expression.num1, expression.num2);
+            console.log(expression.num1);
+    }
+}
 function reset(){expression = {num1 : 0,num2 : 0}}
